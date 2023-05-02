ae63882fec84b43e736aa2d73be398bf28dc5a57
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-05-02 19:25:43 +0800

    closes #9 example for a ternary operator

--- reactapp1/src/App.js
+++ reactapp1/src/App.js
@@ -15,12 +15,32 @@ function App() {
 export default App;
 */
 
+/*
 function App() {
   const name = 'tiff';
   return (
     <>
       <h1>Hello World! React</h1>
       <h2>Welcome {name} to our react crash course!</h2>
+    </>
+  );
+}
+
+export default App;
+*/
+
+function App() {
+  const name = 'tiff';
+  // add const variable and assign a value of false boolean
+  const a = true;
+  const b = 5+5;
+  return (
+    <>
+      <h1>Hello World! React</h1>
+      <h2>Welcome {name} to our react crash course!</h2>
+      <h2>I am new to JSX and I love it! {a ? 'yes': 'no'}</h2>
+      <h2>{5+5}</h2>
+      <h2>{b}</h2>
     </>
   );
 }
