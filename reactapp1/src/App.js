fea29b028aa3a44d96825387115de7f81a505d61
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-05-02 19:06:59 +0800

    closes #7 replace div classname App with fragments

--- reactapp1/src/App.js
+++ reactapp1/src/App.js
@@ -1,14 +1,27 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+/*
 function App() {
   const name = 'tiff';
   return (
     <div className="App">
       <h1>Hello World! React</h1>
-      <h2 className="APP"></h2>
       <h2>Welcome {name} to our react crash course!</h2>
     </div>
+  );
+}
+
+export default App;
+*/
+
+function App() {
+  const name = 'tiff';
+  return (
+    <>
+      <h1>Hello World! React</h1>
+      <h2>Welcome {name} to our react crash course!</h2>
+    </>
   );
 }
 
