922c01c4eb25dd166ea5655a18251e470cc7b8c2
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-05-02 18:37:35 +0800

    closes #8 add js expression example

--- reactapp1/src/App.js
+++ reactapp1/src/App.js
@@ -2,9 +2,11 @@
 // import './App.css';
 
 function App() {
+  const name = 'tiff';
   return (
     <div className="App">
       <h1>Hello World! React</h1>
+      <h2>Welcome {name} to our react crash course!</h2>
     </div>
   );
 }
