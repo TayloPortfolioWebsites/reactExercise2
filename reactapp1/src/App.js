003fd3d14db7ca9b9d3348e67512c4b05f435d56
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-05-02 18:43:20 +0800

    closes #6 add h2 in div with className app

--- reactapp1/src/App.js
+++ reactapp1/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World! React</h1>
+      <h2 className="APP"></h2>
       <h2>Welcome {name} to our react crash course!</h2>
     </div>
   );
