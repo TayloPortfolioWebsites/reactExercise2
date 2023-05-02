25e4aba2039430e84e7f9ab555788d1e7fd4a047
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-05-02 19:39:20 +0800

    closes #10 created components folder in src and added css styles for div container

--- reactapp1/src/App.js
+++ reactapp1/src/App.js
@@ -29,6 +29,7 @@ function App() {
 export default App;
 */
 
+/*
 function App() {
   const name = 'tiff';
   // add const variable and assign a value of false boolean
@@ -42,6 +43,25 @@ function App() {
       <h2>{5+5}</h2>
       <h2>{b}</h2>
     </>
+  );
+}
+
+export default App;
+*/
+
+function App() {
+  const name = 'tiff';
+  // add const variable and assign a value of false boolean
+  const a = true;
+  const b = 5+5;
+  return (
+    <div className="container">
+      <h1>Hello World! React</h1>
+      <h2>Welcome {name} to our react crash course!</h2>
+      <h2>I am new to JSX and I love it! {a ? 'yes': 'no'}</h2>
+      <h2>{5+5}</h2>
+      <h2>{b}</h2>
+    </div>
   );
 }
 
