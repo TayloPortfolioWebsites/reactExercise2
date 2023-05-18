012bba0cff4f69b14c61f95c5c7a92847323c02f
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-05-18 20:38:01 +0800

    delete and add tasks both in ui and database WIP

--- reactapp1/src/App.js
+++ reactapp1/src/App.js
@@ -25,11 +25,26 @@ function App() {
   }
 
 // ADD TASK
+/*
 const addTask = (tasklist) =>{
   const id = Math.floor(Math.random() * 10000) + 1;
   
   const newTask = {id, ...tasklist}
   setTasks([...task, newTask]);
+}
+*/
+
+const addTask = async (tasks) =>{
+  const res = await fetch('http://localhost:5000/tasks',{
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(tasks) 
+  })
+
+  const data = await res.json();
+  setTasks([...tasks, data]);
 }
 
 // DELETE TASK
